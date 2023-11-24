bd73c31a9f41af410a9a9632157e3f53a146413d
Wenqi Zheng <wenqizheng326@gmail.com>
2023-11-24 01:10:35 -0500

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by Wenqi Zheng on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
